b02c67713a87deeb60e2e98c7ee89a581c5828e1
Charles <1320238094@qq.com>
2019-05-10 16:33:28 +0800

    添加注释

--- src/ipcmain/ChannelHandler.ts
+++ src/ipcmain/ChannelHandler.ts
@@ -4,5 +4,10 @@ import {Responser} from './Responser';
  * 处理事件
  */
 export interface ChannelHandler {
+  /**
+   * 处理事件
+   * @param responser
+   * @param param
+   */
   handle(responser: Responser, param: {[key: string]: any});
 }
