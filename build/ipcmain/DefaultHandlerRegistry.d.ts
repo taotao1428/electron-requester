a6d899145a46b50a4206231787da09a5e3f1b25f
Charles <1320238094@qq.com>
2019-05-10 17:06:31 +0800

    修改部分错误

--- build/ipcmain/DefaultHandlerRegistry.d.ts
+++ build/ipcmain/DefaultHandlerRegistry.d.ts
@@ -1,6 +1,6 @@
 import { HandlerRegistry } from "./HandlerRegistry";
 import { ChannelHandler } from "./ChannelHandler";
-export declare abstract class DefaultHandlerRegistry implements HandlerRegistry {
+export declare class DefaultHandlerRegistry implements HandlerRegistry {
     private handlers;
     getHandler(channel: string): ChannelHandler;
     register(channel: string, handler: ChannelHandler): this;
--- build/ipcmain/channelhandler.d.ts
+++ build/ipcmain/channelhandler.d.ts
@@ -3,6 +3,11 @@ import { Responser } from './Responser';
  * 处理事件
  */
 export interface ChannelHandler {
+    /**
+     * 处理事件
+     * @param responser
+     * @param param
+     */
     handle(responser: Responser, param: {
         [key: string]: any;
     }): any;
